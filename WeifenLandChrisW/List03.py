cb3668f7e82dbfc0b3c63ca00a5b878fab81af39
LAPTOP-KV4DTCCF\chris <wenk0003@algonquinlive.com>
2018-01-18 13:41:27 -0500

    no message

---
+++ WeifenLandChrisW/List03.py
@@ -0,0 +1,28 @@
+from sys import argv
+arcpy= None
+def importarcpy():
+    global arcpy
+    if arcpy== None:
+        import arcpy
+        from arcpy import Describe
+        from arcpy import env
+
+if len(argv) != 3:
+    print "List03.py <FeatureClassName> <FeatureType>"
+
+else:
+    importarcpy()
+    working_folder= argv[0]
+    path_to_folder= r"..\..\..\Data"
+    folder= argv[1]
+    arcpy.env.workspace= path_to_folder+folder
+    fctype=arcpy.ListFeatureClasses('',argv[2])
+
+    if arcpy.Exists(working_folder)==True:
+        if len(fctype)>0:
+            for fc in fctype:
+                print fc
+        else:
+            print argv[2]+ "Is not a valid feature type"
+    else:
+        print datafolder +" does not exist."
